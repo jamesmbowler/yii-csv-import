5775a1d5ecf63beff7ee42f4ed5efcd3e9b9934b
james <james@james-Inspiron-5737.(none)>
2014-03-14 12:18:00 -0700

    detab

--- ImportModule.php
+++ ImportModule.php
@@ -8,30 +8,30 @@
  */
 class ImportModule extends CWebModule
 {
-	/**
-	 * @var array associative array of 'scopename' to commentable models 'modelclass'
-	 *
-	 *
-	 * 'modelclass' is a class name of the importable AR
-	 * this AR class must have the {@see CommentableBehavior} attached to it
-	 */
-	public $commentableModels = array();
-
-	/**
-	 * @var string allows you to extend comment class and use your extended one, set path alias here
-	 */
-	public $importModelClass = 'import.models.Import';
+    /**
+     * @var array associative array of 'scopename' to commentable models 'modelclass'
+     *
+     *
+     * 'modelclass' is a class name of the importable AR
+     * this AR class must have the {@see CommentableBehavior} attached to it
+     */
+    public $commentableModels = array();
 
     /**
-	 * @var array you can set filters that will be added to the comment controller {@see CController::filters()}
-	 */
-	public $controllerFilters = array();
+     * @var string allows you to extend comment class and use your extended one, set path alias here
+     */
+    public $importModelClass = 'import.models.Import';
 
-	/**
-	 * @var array you can set accessRules that will be added to the comment controller {@see CController::accessRules()}
-	 */
-	public $controllerAccessRules = array();
-	
+    /**
+     * @var array you can set filters that will be added to the comment controller {@see CController::filters()}
+     */
+    public $controllerFilters = array();
+
+    /**
+     * @var array you can set accessRules that will be added to the comment controller {@see CController::accessRules()}
+     */
+    public $controllerAccessRules = array();
+    
     public function init()
     {
         // this method is called when the module is being created
@@ -39,8 +39,8 @@ class ImportModule extends CWebModule
 
         // import the module-level models and components
         $this->setImport(array(
-	        'import.models.*',
-	        'import.behaviors.*',
+            'import.models.*',
+            'import.behaviors.*',
         ));
     }
 
@@ -56,35 +56,35 @@ class ImportModule extends CWebModule
             return false;
     }
 
-	/**
-	 * This event is raised after a new import has been added
-	 *
-	 * @param $comment
-	 * @param $model
-	 */
-	public function onAfterImport($modelIds, $modelName)
-	{
-		$event = new ImportEvent();
-		$event->modelIds = $modelIds;
-		$event->modelName = $modelName;
-		$this->raiseEvent('onAfterImport', $event);
-	}
-	
-	/**
-	 * This event is raised after a new comment has been added
-	 *
-	 * @param $comment
-	 * @param $model
-	 */
-	
-	public function onBeforeShowForm($model)
-	{
-	    if(method_exists(get_class($model), 'onBeforeShowForm'))
-	    {
-	        $model = $model->onBeforeShowForm($model);
-	    }
-	    return $model;
-	
-	}
+    /**
+     * This event is raised after a new import has been added
+     *
+     * @param $comment
+     * @param $model
+     */
+    public function onAfterImport($modelIds, $modelName)
+    {
+        $event = new ImportEvent();
+        $event->modelIds = $modelIds;
+        $event->modelName = $modelName;
+        $this->raiseEvent('onAfterImport', $event);
+    }
+    
+    /**
+     * This event is raised after a new comment has been added
+     *
+     * @param $comment
+     * @param $model
+     */
+    
+    public function onBeforeShowForm($model)
+    {
+        if(method_exists(get_class($model), 'onBeforeShowForm'))
+        {
+            $model = $model->onBeforeShowForm($model);
+        }
+        return $model;
+    
+    }
 
 }
